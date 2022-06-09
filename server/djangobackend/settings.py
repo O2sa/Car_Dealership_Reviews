a31ee3686bf7e9d00e40c4a85f8c16d8e0b837a8
XiaoSanchez <chengxiaosanchez@gmail.com>
2022-06-09 01:50:41 -0400

    v1.0

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["jms-ibmcloud-bestcarsfull.us-south.cf.appdomain.cloud", "localhost"]
+ALLOWED_HOSTS = ["flaviodiniz.us-south.cf.appdomain.cloud", "localhost"]
 
 
 # Application definition
